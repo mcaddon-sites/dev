29e9f672d8af091088325c0eefc983ef5c6acbed
Legopitstop <1589lego@gmail.com>
2026-06-17 23:56:35 -0500

    Add "Add-On Registry" link to navigation

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -4,6 +4,10 @@ import { addons } from "./addons";
 
 export const nav: DefaultTheme.NavItem[] = [
   {
+    text: "Add-On Registry",
+    link: "https://github.com/Bedrock-OSS/add-on-registry",
+  },
+  {
     text: "Add-ons",
     items: addons.map(({ title, link }) => ({ text: title, link })),
   },
